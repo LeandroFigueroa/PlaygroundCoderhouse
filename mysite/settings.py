a7d7c87879fd9f65cfa7ff524320599749c90968
Leandro Figueroa <105681474+LeandroFigueroa@users.noreply.github.com>
2022-07-02 20:18:18 -0300

    test2

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'america/buenos_aires'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
